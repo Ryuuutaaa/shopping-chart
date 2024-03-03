8311e1f0019639cd05a17f63673aa8880273610a
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2024-03-04 01:52:48 +0800

    navbar done

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Store from "./pages/Store";
 import About from "./pages/About";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
     <>
+      <Navbar />
       <Container className="mb-4">
         <Routes>
           <Route path="/" element={<Home />} />
