ecb6e829ca4bb5ff286f6e9192ecab03e86fbf0e
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2024-03-04 02:25:58 +0800

    not done part store

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,7 +1,21 @@
 import React from "react";
+import storeItems from "../data/items.json";
+import { Col, Row } from "react-bootstrap";
+import StoreItem from "../components/StoreItem";
 
 const Store = () => {
-  return <div>Store</div>;
+  return (
+    <div>
+      <h1>Store</h1>
+      <Row>
+        {storeItems.map((item) => (
+          <Col key={item.id}>
+            <StoreItem {...item} />
+          </Col>
+        ))}
+      </Row>
+    </div>
+  );
 };
 
 export default Store;
---
+++ src/utilities/FormatCurrency.ts
@@ -0,0 +1,10 @@
+const CURRENCY_FORMATER = new Intl.NumberFormat(undefined, {
+  currency: "USD",
+  style: "currency",
+});
+
+const FormatCurrency = (number: number) => {
+  return CURRENCY_FORMATER.format(number);
+};
+
+export default FormatCurrency;
