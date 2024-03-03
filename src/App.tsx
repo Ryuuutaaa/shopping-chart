2ed7a877d813ab5423fba9800ca12e12dc9f9e8c
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2024-03-03 21:50:30 +0800

    route

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
+import { Container } from "react-bootstrap";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Store from "./pages/Store";
+import About from "./pages/About";
+
 const App = () => {
   return (
     <>
-      <h1>haiii</h1>
+      <Container className="mb-4">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/store" element={<Store />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Container>
     </>
   );
 };
