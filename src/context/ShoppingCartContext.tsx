15d7fb75eb34f9f0c482a149ee4884d0966a2bee
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2024-03-04 15:11:27 +0800

    shopping cart provider

---
+++ src/context/ShoppingCartContext.tsx
@@ -0,0 +1,21 @@
+import React, { ReactNode, createContext, useContext } from "react";
+
+type ShoppingCartProviderProps = {
+  children: ReactNode;
+};
+
+const ShoppingCartContext = createContext({});
+
+export const useShoppingCartContext = () => {
+  return useContext(ShoppingCartContext);
+};
+
+export const ShoppingCartProvider = ({
+  children,
+}: ShoppingCartProviderProps) => {
+  return (
+    <ShoppingCartContext.Provider value={{}}>
+      {children}
+    </ShoppingCartContext.Provider>
+  );
+};
